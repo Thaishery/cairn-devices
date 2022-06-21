2cec45eabce960d25685e0c329e05c1619deda0b
thaishery <gdeb@gdeb.fr>
2022-06-22 01:42:03 +0200

    révision carousel

--- asset/js/module/carousel.js
+++ asset/js/module/carousel.js
@@ -1,28 +1,30 @@
 export default function carousel(){
-  const carouselsItems = document.getElementsByClassName('carousel__item')
+  const carouselsImages = document.getElementsByClassName('carouselle__item__image__container ')
+  const dots = document.getElementsByClassName('carousell__iteme__botom_dot')
   const rightArrow = document.getElementsByClassName('carouselle__item__right__arrow')
   const leftArrow = document.getElementsByClassName('carouselle__item__left__arrow')
+  var curPos = 0;
+
+  setInterval(nextSlide, 10*1000)
+
+  rightArrow[0].addEventListener("click",nextSlide)
+  leftArrow[0].addEventListener("click",previousSlide)
   
-  for(let i = 0; i< rightArrow.length; i++){
-    rightArrow[i].addEventListener('click',function(){mouveCarouselRight(i)})
+  function nextSlide(){
+    carouselsImages[curPos].classList.remove('--active')
+    dots[curPos].classList.remove('--active')
+    if(curPos < carouselsImages.length -1 ) curPos++
+    else curPos = 0;
+    carouselsImages[curPos].classList.add('--active')
+    dots[curPos].classList.add('--active')
   }
-  for(let i = rightArrow.length-1; i >= 0; i --){
-    leftArrow[i].addEventListener('click',function(){mouveCarouselLeft(i)})
-  }
-  function mouveCarouselRight(curentItem){
-    carouselsItems[curentItem].className = "carousel__item carousel__item--disactive"
-    if(curentItem === (carouselsItems.length-1)){
-      carouselsItems[0].className = "carousel__item carousel__item--active"
-    }else{
-    carouselsItems[curentItem+1].className = "carousel__item carousel__item--active"
-    }
-  }
-  function mouveCarouselLeft(curentItem){
-    carouselsItems[curentItem].className = "carousel__item carousel__item--disactive"
-    if(curentItem === 0){
-      carouselsItems[carouselsItems.length-1].className = "carousel__item carousel__item--active"
-    }else{
-    carouselsItems[curentItem-1].className = "carousel__item carousel__item--active"
-    }
+  
+  function previousSlide(){
+    carouselsImages[curPos].classList.remove('--active')
+    dots[curPos].classList.remove('--active')
+    if(curPos > 0 ) curPos --
+    else curPos = carouselsImages.length -1;
+    carouselsImages[curPos].classList.add('--active')
+    dots[curPos].classList.add('--active')
   }
 }
