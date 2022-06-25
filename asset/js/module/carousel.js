45fa930a459eb23f6492e7fe34fe43cc90335358
thaishery <gdeb@gdeb.fr>
2022-06-25 14:04:00 +0200

    style carousel ok

--- asset/js/module/carousel.js
+++ asset/js/module/carousel.js
@@ -1,32 +1,36 @@
-export default function carousel(){
-  const carouselsImages = document.getElementsByClassName('carouselle__item__image__container ')
-  const dots = document.getElementsByClassName('carousell__iteme__botom_dot')
-  const rightArrow = document.getElementsByClassName('carouselle__item__right__arrow')
-  const leftArrow = document.getElementsByClassName('carouselle__item__left__arrow')
+export default function carousel() {
+  const carouselsImages = document.getElementsByClassName("carouselle__image");
+  const dots = document.getElementsByClassName("carouselle__botomDot");
+  const rightArrow = document.getElementsByClassName("carouselle__rightArrow");
+  const leftArrow = document.getElementsByClassName("carouselle__leftArrow");
   var curPos = 0;
 
-  const timedNextSlide = setInterval(nextSlide, 10*1000)
+  const timedNextSlide = setInterval(nextSlide, 10 * 1000);
 
-  rightArrow[0].addEventListener("click",(e)=>{nextSlide(e)})
-  leftArrow[0].addEventListener("click",(e)=>{previousSlide(e)})
-  
-  function nextSlide(e){
-    if(e)clearInterval(timedNextSlide)
-    carouselsImages[curPos].classList.remove('--active')
-    dots[curPos].classList.remove('--active')
-    if(curPos < carouselsImages.length -1 ) curPos++
+  rightArrow[0].addEventListener("click", (e) => {
+    nextSlide(e);
+  });
+  leftArrow[0].addEventListener("click", (e) => {
+    previousSlide(e);
+  });
+
+  function nextSlide(e) {
+    if (e) clearInterval(timedNextSlide);
+    carouselsImages[curPos].classList.remove("carouselle__image--active");
+    dots[curPos].classList.remove("carouselle__botomDot--active");
+    if (curPos < carouselsImages.length - 1) curPos++;
     else curPos = 0;
-    carouselsImages[curPos].classList.add('--active')
-    dots[curPos].classList.add('--active')
+    carouselsImages[curPos].classList.add("carouselle__image--active");
+    dots[curPos].classList.add("carouselle__botomDot--active");
   }
 
-  function previousSlide(e){
-    if(e)clearInterval(timedNextSlide)
-    carouselsImages[curPos].classList.remove('--active')
-    dots[curPos].classList.remove('--active')
-    if(curPos > 0 ) curPos --
-    else curPos = carouselsImages.length -1;
-    carouselsImages[curPos].classList.add('--active')
-    dots[curPos].classList.add('--active')
+  function previousSlide(e) {
+    if (e) clearInterval(timedNextSlide);
+    carouselsImages[curPos].classList.remove("carouselle__image--active");
+    dots[curPos].classList.remove("carouselle__botomDot--active");
+    if (curPos > 0) curPos--;
+    else curPos = carouselsImages.length - 1;
+    carouselsImages[curPos].classList.add("carouselle__image--active");
+    dots[curPos].classList.add("carouselle__botomDot--active");
   }
 }
