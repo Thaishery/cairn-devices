aadc5f6a70a0d60cfcd29a181ddd1ca39bd3cddb
thaishery <gdeb@gdeb.fr>
2022-06-26 15:46:33 +0200

    header okish, carousel okish, début articles

--- asset/js/module/menu.js
+++ asset/js/module/menu.js
@@ -1,17 +1,19 @@
-export default function menu(){
-  const menu = document.getElementsByClassName("header__burger")
-  const listHiden = document.getElementsByClassName("header__burger__list_hiden")
-  const listShow = document.getElementsByClassName("header__burger__list_active")
-  const menuLogoShow = document.getElementsByClassName("header__burger__drawer_actif")
-  const menuLogoHiden = document.getElementsByClassName("header__burger__drawer_hiden")
+export default function menu() {
+  const menu = document.getElementsByClassName("header__nav");
+  const listHiden = document.getElementsByClassName("navHeader__ul--hiden");
+  const listShow = document.getElementsByClassName("navHeader__ul--active");
+  const menuLogoShow = document.getElementsByClassName("navHeader__nav--actif");
+  const menuLogoHiden = document.getElementsByClassName(
+    "navHeader__nav--hiden"
+  );
 
-  menu[0].addEventListener("click",(e)=>{
-    if(listHiden[0]){
-    listHiden[0].className = "header__burger__list_active"
-    menuLogoShow[0].className = "header__burger__drawer_hiden"
-    }else{
-      listShow[0].className = "header__burger__list_hiden"
-      menuLogoHiden[0].className = "header__burger__drawer_actif"
+  menu[0].addEventListener("click", (e) => {
+    if (listHiden[0]) {
+      listHiden[0].className = "navHeader__ul--active";
+      menuLogoShow[0].className = "navHeader__nav--hiden";
+    } else {
+      listShow[0].className = "navHeader__ul--hiden";
+      menuLogoHiden[0].className = "navHeader__nav--actif";
     }
-  })
+  });
 }
